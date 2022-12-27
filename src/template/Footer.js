56cfa34fbfe6eca31c3691c31b6854926a4191c1
Patrícia Andrade <patricia.andrade094@gmail.com>
2022-12-27 19:06:00 -0300

    first commit

---
+++ src/template/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer style={{backgroundColor:"#6f42c1"}} className="mt-auto py-5">
+      <div className="container d-flex justify-content-center">
+        <span style={{color:"#FFFFFF"}} className="text">Todos os direitos reservados &copy; KawaiiYou 2022</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
